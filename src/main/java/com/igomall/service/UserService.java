7e59d7ca905f5440ac718702b8a5d2a7a6ed88e4
1169794338@qq.com <Liziyi521521>
2019-04-16 15:26:08 +0800

    升级boot版本到2.1.4

--- src/main/java/com/igomall/service/UserService.java
+++ src/main/java/com/igomall/service/UserService.java
@@ -1,8 +1,4 @@
-/*
- * Copyright 2005-2017 shopxx.net. All rights reserved.
- * Support: http://www.shopxx.net
- * License: http://www.shopxx.net/license
- */
+
 package com.igomall.service;
 
 import java.util.Set;
